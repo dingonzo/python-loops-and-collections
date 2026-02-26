d43fc30cbbcc7ef0af8da5c2ca00ad441bf2be64
Edgar Gonzalez <245eddy.g@gmail.com>
2026-02-26 00:03:56 -0500

    Create test-python-loops-and-collections.py

---
+++ test-python-loops-and-collections.py
@@ -0,0 +1,6 @@
+import python-loops-and-collections # Ensure this matches your filename exactly
+
+def test_collections():
+    # This ensures your lists actually exist after the script runs
+    assert len(python_loops.dailyTemps) == 5
+    assert len(python_loops.vehicleInventory) == 3
