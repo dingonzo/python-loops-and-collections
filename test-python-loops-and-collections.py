31342634ec3a0e09775477252f0abddb41393aa8
Edgar Gonzalez <245eddy.g@gmail.com>
2026-02-26 00:07:53 -0500

    Update test-python-loops-and-collections.py

--- test-python-loops-and-collections.py
+++ test-python-loops-and-collections.py
@@ -1,6 +1,6 @@
 import python_loops_and_collections # Ensure this matches your filename exactly
 
 def test_collections():
-    # This ensures your lists actually exist after the script runs
-    assert len(python_loops.dailyTemps) == 5
-    assert len(python_loops.vehicleInventory) == 3
+    # Update the prefix here to match the import above!
+    assert len(python_loops_and_collections.dailyTemps) == 5
+    assert len(python_loops_and_collections.vehicleInventory) == 3
